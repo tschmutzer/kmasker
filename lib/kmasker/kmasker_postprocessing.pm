5c73bec1a85976aa8add9c5df1a7fd7b4ac0f1b6
tschmutzer <tschmutzer@users.noreply.github.com>
2016-10-04 14:32:26 +0200

    First version of kmasker_postprocessing.pl module.

---
+++ lib/kmasker/kmasker_postprocessing.pm
@@ -0,0 +1,40 @@
+package kmasker::kmasker_postprocessing;
+use Exporter qw(import);
+use File::Basename;
+use strict;
+use warnings;
+
+our @ISA = qw(Exporter);
+our @EXPORT = qw(
+plot_histogram
+repeat_annotation
+gff_construction
+);
+our @EXPORT_OK = qw(plot_histogram repeat_annotation gff_construction);
+
+
+## subroutine
+#
+sub plot_histogram(){
+	
+}
+
+
+## subroutine
+#
+sub gff_construction{
+#implement
+	
+}
+
+## subroutine
+#
+sub repeat_annotation{
+#implement	
+
+}
+
+
+
+
+1;
