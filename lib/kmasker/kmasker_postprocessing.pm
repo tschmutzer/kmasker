d96f382864a54903785541c6f010cca50d383243
culpinnis <culpinnis@gmail.com>
2016-10-05 23:29:40 +0200

    Added code for the plotting subroutine in the postprocessing module of kmaskers frontend.

--- lib/kmasker/kmasker_postprocessing.pm
+++ lib/kmasker/kmasker_postprocessing.pm
@@ -15,8 +15,16 @@ our @EXPORT_OK = qw(plot_histogram repeat_annotation gff_construction);
 
 ## subroutine
 #
-sub plot_histogram(){
-	
+sub plot_histogram{
+    $occ=$_[0];
+    $clist=$_[1];
+    system("occVisualizer.R -i ".$occ." -l ".$clist);
+    #The script will skip large contigs to avoid long running times
+    #You can force it to do it anyway with -f
+    #If you do not want to provide a contig list (instead running on all entries
+    #in the occ file) use the following
+    #system("occVisualizer.R -i ".$occ.");
+    #The script will generate one plot per contig in the current working directory
 }
 
 
