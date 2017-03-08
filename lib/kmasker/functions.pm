a179f39b1679bd5497d62a3c0a19266d07a88704
tschmutzer <tschmutzer@users.noreply.github.com>
2017-03-08 10:41:12 +0100

    added date to spot last update

--- lib/kmasker/functions.pm
+++ lib/kmasker/functions.pm
@@ -7,6 +7,8 @@ our @EXPORT = qw(read_write_repository);
 our @EXPORT_OK = qw(read_write_repository);
 
 
+## VERSION
+my $version_PM_functions 	= "0.0.1 rc170308";
 
 sub add_repository {
    # function adds local repository to global shared repository 
