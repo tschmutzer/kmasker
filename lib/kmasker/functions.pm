f889fdc4d32b4caa60b5547ecbba1a17bc7ed7d5
tschmutzer <tschmutzer@users.noreply.github.com>
2017-03-10 14:37:35 +0100

    update functions

--- lib/kmasker/functions.pm
+++ lib/kmasker/functions.pm
@@ -1,10 +1,12 @@
 package kmasker::functions;
+use kmasker::occ;
+use kmasker::filehandler;
 use Exporter qw(import);
 use strict;
 use warnings;
 our @ISA = qw(Exporter);
-our @EXPORT = qw(read_write_repository Xtract);
-our @EXPORT_OK = qw(read_write_repository Xtract);
+our @EXPORT = qw(read_write_repository Xtract add_annotation);
+our @EXPORT_OK = qw(read_write_repository Xtract add_annotation);
 
 
 ## VERSION
@@ -29,9 +31,19 @@ sub read_write_repository {
 	
 }
 
+sub add_annotation {
+        my $FASTA = $_[0]; #FASTA to extract sequences from
+        my $TAB = $_[1]; #TAB-file with regions to extract
+        my $BLAST_db = $_[2]; #BLAST-reference
+        my $GFF = $_[3]; #GFF-file to be annotated
+        #FIXME: Give configuration hash as 5th parameter.
 
-################################
-# subroutin 
+        extract_sequence_region($FASTA, $TAB);
+        system("blastn -db " . $BLAST_db . " -query " . "selected_" . $FASTA . " -perc_identity 80 -word_size 50 -evalue 0.1 -num_threads 30 -outfmt 6 -ungapped -max_hsps 1 -max_target_seqs 1" . " -out kmasker_blast.txt");
+        #FIXME: Add exchangeable configuration to blast
+        add_annotation_to_gff($GFF, "kmasker_blast.txt");
+}
+
 sub Xtract(){
 	
 	my $fasta 		= $_[0];
