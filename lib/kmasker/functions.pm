5173317a39ecb956f2c8bfc0068af5396b56ed50
culpinnis <culpinnis@gmail.com>
2017-03-10 13:08:24 +0100

    Version tags; EXPORT lists

--- lib/kmasker/functions.pm
+++ lib/kmasker/functions.pm
@@ -3,12 +3,12 @@ use Exporter qw(import);
 use strict;
 use warnings;
 our @ISA = qw(Exporter);
-our @EXPORT = qw(read_write_repository);
-our @EXPORT_OK = qw(read_write_repository);
+our @EXPORT = qw(read_write_repository add_annotation);
+our @EXPORT_OK = qw(read_write_repository add_annotation);
 
 
 ## VERSION
-my $version_PM_functions 	= "0.0.1 rc170308";
+my $version_PM_functions 	= "0.0.1 rc170310";
 
 sub add_repository {
    # function adds local repository to global shared repository 
@@ -23,7 +23,6 @@ sub show_repository {
 
 sub read_write_repository {
 	#read config 
-	
 	#my $usr = `echo \$USER`;
 	my $usr = $ENV{"USER"};
 	print "\n USER is: ".$usr;
@@ -35,8 +34,10 @@ sub add_annotation {
 	my $TAB = $_[1]; #TAB-file with regions to extract
 	my $BLAST_db = $_[2]; #BLAST-reference 
 	my $GFF = $_[3]; #GFF-file to be annotated
+	#FIXME: Give configuration hash as 5th parameter. 
 
 	extract_sequence_region($FASTA, $TAB);
-	system("blastn -db " . $BLAST_db . " -query " . "selected_" . $FASTA . "-perc_identity 80 -word_size 50 -evalue 0.1 -num_threads 30 -outfmt 6 -ungapped -max_hsps 1 -max_target_seqs 1" . ">kmasker_blast.txt");
+	system("blastn -db " . $BLAST_db . " -query " . "selected_" . $FASTA . "-perc_identity 80 -word_size 50 -evalue 0.1 -num_threads 30 -outfmt 6 -ungapped -max_hsps 1 -max_target_seqs 1" . "-out kmasker_blast.txt");
+	#FIXME: Add exchangeable configuration to blast
 	add_annotation_to_gff($GFF, "kmasker_blast.txt");
 	}
