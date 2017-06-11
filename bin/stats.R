c2fd022754010b39a8d35eb0f211ab09f98bbf90
culpinnis <culpinnis@gmail.com>
2017-06-11 21:41:39 +0200

    New working skript for the statistics of gff features and occ

--- bin/stats.R
+++ bin/stats.R
@@ -6,7 +6,8 @@ pacman::p_load(getopt, zoo, scales, stringr)
 spec=matrix(c(
   'input', 'i', 1, "character",
   'help', 'h', 0, "logical",
-  'gff', 'g', 2, "character"
+  'gff', 'g', 1, "character",
+  'class', 'c', 1, "character" 
 ), byrow=TRUE, ncol=4)
 opt=getopt(spec)
 if ( !is.null(opt$help) ) {
@@ -31,7 +32,7 @@ gffRead <- function(gffFile, nrows = -1) {
 
 ####
 #choose the feature (contig, KRC; KRR)
-feature<-"Sequence_Feature"
+feature<-opt$class
 gff<-gffRead(opt$gff)
 gff_feature<-gff[which(gff[,"feature"]==feature),]
 #pharse quality-data
@@ -45,6 +46,7 @@ opentags<-which(grepl(">.*", qual_file) == TRUE)
 #}
 #id_list<-str_trim(id_list)
 id_list<-gff_feature[,"seqname"]
+first<-0
 for (pos in opentags){
   if(str_split(str_trim(str_sub(qual_file[pos],2)), " ")[[1]][1] %in% id_list) {
     #get the ID out of the whole line 
@@ -58,25 +60,33 @@ for (pos in opentags){
       #Then we split the string into a list with a whitespace as sperator
       #After that we turn the list into a vector
       #We convert the characters into numbers finaly.
-      approx<-(next_pos_in_text-pos)*30
-      if(approx > 300000) {
-        print("Sorry, your contig seems to be too large to be read in. Anyway, you can force me to read it with --force")
-        next;
-      }
+     # approx<-(next_pos_in_text-pos)*30
+     # if(approx > 300000) {
+    #    print("Sorry, your contig seems to be too large to be read in. Anyway, you can force me to read it with --force")
+    #    next;
+   #   }
       #Our OCC format specification definies 80 characters per line at maximum (but mostly they are about 30 long)
       #This check will be tricked out by a file which is not in the specification
       occs<-as.numeric(unlist(strsplit(qual_file[(pos+1):(next_pos_in_text[1] - 1)], " ")))
     }
     else{
       next_pos_in_text <- length(qual_file)
-      approx<-(next_pos_in_text-pos)*30
-      if(approx > 300000) {
-        print("Sorry, your contig seems to be too large to be read in. Anyway, you can force me to read it with --force")
-        next;
-      }
+    #  approx<-(next_pos_in_text-pos)*30
+     # if(approx > 300000) {
+    #    print("Sorry, your contig seems to be too large to be read in. Anyway, you can force me to read it with --force")
+     #   next;
+   #   }
       occs<-as.numeric(unlist(strsplit(qual_file[(pos+1):(next_pos_in_text)], " ")))
-      output<-data.frame(Min = min(occs), Max=max(occs), Avg=mean(occs), "zero_pos"=length(occs[occs==0]), "Q25"=quantile(occs, 0.25)[[1]], "Q50"=quantile(occs, 0.25)[[1]], "Q75"=quantile(occs, 0.25)[[1]])
-     #write output here
+    }
+    output<-data.frame(Name = id , Start=gff_feature$start, End=gff_feature$end, Min = min(occs), Max=max(occs), Avg=mean(occs), "zero_pos"=length(occs[occs==0]), "Q25"=quantile(occs, 0.25)[[1]], "Q50"=quantile(occs, 0.25)[[1]], "Q75"=quantile(occs, 0.25)[[1]])
+    #write output here
+    print(output)
+    if(first == 0){
+      write.table(output, file=paste("Stats_", feature, "_", opt$gff , ".tab", sep="" ), quote=FALSE, row.names = FALSE, append=FALSE)
+      first<-1
+    }
+    else{
+      write.table(output, file=paste("Stats_", feature, "_", opt$gff , ".tab", sep="" ), quote=FALSE, row.names = FALSE, col.names = FALSE, append=TRUE)
     }
   }
 }
