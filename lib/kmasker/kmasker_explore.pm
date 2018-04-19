fbb0c1eba533ec36684e0bba5d07469b298b8714
tschmutzer <thomas.schmutzer@web.de>
2018-04-19 18:11:13 +0200

    Major update. [rename prostprocessing to explore, delete repeat variables, added container function 'custom_annotate']

---
+++ lib/kmasker/kmasker_explore.pm
@@ -0,0 +1,99 @@
+package kmasker::kmasker_explore;
+use Exporter qw(import);
+use File::Basename;
+use strict;
+use warnings;
+
+our @ISA = qw(Exporter);
+our @EXPORT = qw(
+plot_histogram
+repeat_annotation
+gff_construction
+);
+our @EXPORT_OK = qw(plot_histogram custom_annotation gff_construction);
+
+## VERSION
+my $version_PM_explore 	= "0.0.1 rc180419";
+
+## subroutine
+#
+sub plot_histogram{
+    my $occ		=	$_[0];
+    my $clist	=	$_[1];
+    system("occVisualizer.R -i ".$occ." -l ".$clist);
+    #The script will skip large contigs to avoid long running times
+    #You can force it to do it anyway with -f
+    #If you do not want to provide a contig list (instead running on all entries
+    #in the occ file) use the following
+    #system("occVisualizer.R -i ".$occ.");
+    #The script will generate one plot per contig in the current working directory
+    
+    #
+    my @ARRAY_info = split("_", $occ);
+    my $kindex = $ARRAY_info[1];
+    if(!( -d "./Kmasker_plots_".$kindex)){
+    	system("mkdir Kmasker_plots_".$kindex);
+    }
+   
+   	my $LIST = new IO::File($clist, "r") or die "\n unable to read $clist $!";	
+   	while(<$LIST>){
+   		next if($_ =~ /^$/);
+		next if($_ =~ /^#/);
+		my $line = $_;
+		$line =~ s/\n//;
+		system("mv ".$line."*.png Kmasker_plots_".$kindex);
+   	}    
+}
+
+
+## subroutine
+#
+sub gff_construction{
+#implement
+	
+}
+
+## subroutine
+#
+sub repeat_annotation{
+#implement	
+
+}
+
+## subroutine
+#
+sub custom_annotation{
+	my $fasta		=	$_[0];
+    my $gff			=	$_[1];
+    my $href_DB 	=   $_[2]; 
+    my $href_info	=	$_[3];
+    
+    my %HASH_info 	= %{$href_info};
+    my %HASH_DB		= %{$href_DB};
+    my $db_fasta;
+    my $db;
+    
+    if(exists $HASH_DB{"db_fasta"}){
+    	$db_fasta = $HASH_DB{"db_fasta"};
+    }elsif(exists $HASH_DB{"db"}){
+    	$db = $HASH_DB{"db"};    	
+    	# check if file is a blastable DB
+		# assigned to @Chris
+    	
+    }else{
+    	print "\n\n Parameter dbfasta or db have to be provided for custom annotation!";
+    	print   "\n Kmasker was stopped.\n\n";
+    	exit();
+    }
+    
+    
+    # TASK for Chris to move annotation from run to this section!
+	
+	
+}
+
+
+
+
+
+1;
