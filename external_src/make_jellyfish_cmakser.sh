69f4f5b912bbbc61f672f1dcbebee97598959bb9
Chris Ulpinnis <culpinins@gmail.com>
2019-02-18 18:10:42 +0100

    Changes to jellfish script

--- external_src/make_jellyfish_cmakser.sh
+++ external_src/make_jellyfish_cmakser.sh
@@ -7,7 +7,7 @@ cd /io/external/jellyfish/
 env CFLAGS="$STATICLIB_CFLAGS" CXXFLAGS="$STATICLIB_CXXFLAGS"   ./configure --prefix=/hbb_exe/ --disable-shared --enable-static
 make
 make install
-ln -s /hbb_exe/include/jellyfish-2.2.10/jellyfish /hbb_exe/include/jellyfish
+#ln -s /hbb_exe/include/jellyfish-2.2.10/jellyfish /hbb_exe/include/jellyfish
 
 cd /io/src
 scons
