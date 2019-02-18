c1a71eeb3d7e9ff419ab849b2f62e2c8895db3c3
Chris Ulpinnis <culpinins@gmail.com>
2019-02-18 19:18:36 +0100

    Changed Jellyfish back to source code release

--- make_binaries.sh
+++ make_binaries.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cp -R external_src/jellyfish-2.2.10 external_src/jellyfish
+wget -c https://github.com/gmarcais/Jellyfish/releases/download/v2.2.10/jellyfish-2.2.10.tar.gz
+tar -xvf jellyfish-2.2.10.tar.gz
+mv jellyfish-2.2.10 external_src/jellyfish
 docker run -t -i --rm -v `pwd`/src:/io/src -v `pwd`/external_src:/io/external kmasker/builder bash /io/external/make_jellyfish_cmakser.sh
 docker run -t -i --rm -v `pwd`/src:/io/src -v `pwd`/external_src:/io/external kmasker/builder bash /io/external/make_fastqstats.sh
 docker run -t -i --rm -v `pwd`/src:/io/src -v `pwd`/external_src:/io/external kmasker/builder bash /io/external/make_gffread.sh
