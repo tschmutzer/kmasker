301f882bef83e8593c70f725d0cf50a4c2a00f11
Chris Ulpinnis <culpinins@gmail.com>
2019-03-06 11:08:11 +0100

    Changes for Release 1.0

--- make_binaries.sh
+++ make_binaries.sh
@@ -2,6 +2,7 @@
 wget -c https://github.com/gmarcais/Jellyfish/releases/download/v2.2.10/jellyfish-2.2.10.tar.gz
 tar -xvf jellyfish-2.2.10.tar.gz
 mv jellyfish-2.2.10 external_src/jellyfish
+rm -rf jellyfish-2.2.10.tar.gz
 docker run -t -i --rm -v `pwd`/src:/io/src -v `pwd`/external_src:/io/external kmasker/builder bash /io/external/make_jellyfish_cmakser.sh
 docker run -t -i --rm -v `pwd`/src:/io/src -v `pwd`/external_src:/io/external kmasker/builder bash /io/external/make_fastqstats.sh
 docker run -t -i --rm -v `pwd`/src:/io/src -v `pwd`/external_src:/io/external kmasker/builder bash /io/external/make_gffread.sh
