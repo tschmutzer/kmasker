2dd723987fe574e69feb1c5da258464859b7ea40
culpinnis <culpinnis@gmail.com>
2019-10-01 17:49:06 +0200

    Changed static shebangs and python calls

--- lib/perl5/kmasker/kmasker_run.pm
+++ lib/perl5/kmasker/kmasker_run.pm
@@ -402,10 +402,10 @@ sub run_krispr(){
 	
 	#SINGLE SEQ
 	# not activated in Kmasker
-	# system("python3.5 ".$path."/krispr.py single -q ".$krispr_sequence." -j ".$full_kindex_name." -m ".$kripr_mismatch." -c ".$kripr_coverage_threshold);
+	# system("python3 ".$path."/krispr.py single -q ".$krispr_sequence." -j ".$full_kindex_name." -m ".$kripr_mismatch." -c ".$kripr_coverage_threshold);
 	
 	#MULTI FASTA
-	system("python3.5 ".$path."/../krispr/krispr.py multi -q ".$krispr." -j ".$full_kindex_name." -m ".$kripr_mismatch." -c ".$kripr_coverage_threshold." -t >".$OUT_krispr . " 2>&1");
+	system("python3 ".$path."/../krispr/krispr.py multi -q ".$krispr." -j ".$full_kindex_name." -m ".$kripr_mismatch." -c ".$kripr_coverage_threshold." -t >".$OUT_krispr . " 2>&1");
 	print "\n\n ... Kmasker krispr module finished \n";
 	
 	#CLEAN
--- share/kmasker/scripts/FASTA_getContigLength.pl
+++ share/kmasker/scripts/FASTA_getContigLength.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl -w
 use strict;
 use IO::File;
 use File::Basename;
